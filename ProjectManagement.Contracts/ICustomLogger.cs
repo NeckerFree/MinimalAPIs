e63344fd1aa28d2d4881026673304df5b626249c
Elio Cortés <elio.cortes3000@gmail.com>
2025-02-19 14:15:53 -0500

    Added Serilog packages and configured appsetting

---
+++ ProjectManagement.Contracts/ICustomLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPProjectManagement.Contracts
+{
+    public interface ICustomLogger
+    {
+        void LogDebug(string message);
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
